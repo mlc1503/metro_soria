98ee0c8077c4c26610a1ac65ed77121f757865ee
mlc1503 <manelagunas1503@gmail.com>
2025-03-20 05:35:24 +0100

    pequeños pasos

--- src/main.py
+++ src/main.py
@@ -1,19 +1,59 @@
 import flet as ft
-import styles as css
+from styles import PRIMARY_BLUE, PRIMARY_CYAN, SECONDARY_CYAN, ALERT_RED, MAIN_BLACK, MAIN_WHITE, SLIGHT_BLUE
+
 
 
 def main(page: ft.Page):
-    
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                content=ft.ElevatedButton("Page theme button"),
-                bgcolor=css.PRIMARY_BLUE,
-                padding=20,
-                width=300,
-            ),
-        )
-    )
+    page.title = "Metro Soria"
 
+    def route_change(e: ft.RouteChangeEvent) -> None:
+        page.views.clear()
+
+        page.views.append(
+            ft.View(
+                route= '/',
+                controls=[
+                    
+                    ft.AppBar(
+                        title=ft.Text("Hola", color= MAIN_WHITE),
+                        bgcolor=PRIMARY_BLUE,
+                    ),
+                    ft.Column(
+                        horizontal_alignment= ft.MainAxisAlignment.CENTER,
+                        controls=[
+                            ft.TextField(
+                                label=ft.Text("Origen", color= PRIMARY_BLUE),
+                                border_color=PRIMARY_BLUE,
+                            ),
+                            ft.TextField(
+                                label=ft.Text("Destino", color= PRIMARY_BLUE),
+                                border_color=PRIMARY_BLUE,
+                            ),
+                            ft.ElevatedButton(
+                                text="BUSCAR",
+                                color=MAIN_WHITE,
+                                bgcolor=PRIMARY_BLUE,
+                                
+                            )
+                        ]
+                    ),
+                ]
+            )
+        )
+
+        if page.route == '/lineas':
+            page.views.append(
+                ft.View(
+                    route= '/lineas', 
+                    controls=[
+                        ft.Text("colega estamos en lineas")
+                    ]
+                )
+            )
+
+        page.update()
+
+    page.on_route_change = route_change
+    page.go(page.route)
 
 ft.app(main)
