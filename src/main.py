13ea4a828e3ddb782103fb1bbe08db4814147f2b
mlc1503 <manelagunas1503@gmail.com>
2025-03-20 03:55:05 +0100

    starting styling

--- src/main.py
+++ src/main.py
@@ -1,24 +1,17 @@
 import flet as ft
+import styles as css
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+    
     page.add(
         ft.SafeArea(
             ft.Container(
-                counter,
-                alignment=ft.alignment.center,
+                content=ft.ElevatedButton("Page theme button"),
+                bgcolor=css.PRIMARY_BLUE,
+                padding=20,
+                width=300,
             ),
-            expand=True,
         )
     )
 
