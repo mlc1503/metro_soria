2f5270d43f813621dc7abb9075496ed3dd43b6cc
mlc1503 <manelagunas1503@gmail.com>
2025-03-21 02:08:24 +0100

    routing simple. faltan vistas

--- src/main.py
+++ src/main.py
@@ -1,59 +1,34 @@
 import flet as ft
 from styles import PRIMARY_BLUE, PRIMARY_CYAN, SECONDARY_CYAN, ALERT_RED, MAIN_BLACK, MAIN_WHITE, SLIGHT_BLUE
 
+import flet as ft
 
 
 def main(page: ft.Page):
-    page.title = "Metro Soria"
+    page.title = "Calc App"
+    
 
-    def route_change(e: ft.RouteChangeEvent) -> None:
-        page.views.clear()
+    class TestWidget(ft.Column):
+        def __init__(self, text):
+            super().__init__()
+            self.controls = [
+                ft.Text(text, color=MAIN_WHITE)
+            ]
 
-        page.views.append(
-            ft.View(
-                route= '/',
-                controls=[
-                    
-                    ft.AppBar(
-                        title=ft.Text("Hola", color= MAIN_WHITE),
-                        bgcolor=PRIMARY_BLUE,
-                    ),
-                    ft.Column(
-                        horizontal_alignment= ft.MainAxisAlignment.CENTER,
-                        controls=[
-                            ft.TextField(
-                                label=ft.Text("Origen", color= PRIMARY_BLUE),
-                                border_color=PRIMARY_BLUE,
-                            ),
-                            ft.TextField(
-                                label=ft.Text("Destino", color= PRIMARY_BLUE),
-                                border_color=PRIMARY_BLUE,
-                            ),
-                            ft.ElevatedButton(
-                                text="BUSCAR",
-                                color=MAIN_WHITE,
-                                bgcolor=PRIMARY_BLUE,
-                                
-                            )
-                        ]
-                    ),
-                ]
-            )
-        )
-
-        if page.route == '/lineas':
-            page.views.append(
-                ft.View(
-                    route= '/lineas', 
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                bgcolor=ft.colors.BLACK,
+                border_radius=ft.border_radius.all(20),
+                padding=20,
+                content=ft.Column(
                     controls=[
-                        ft.Text("colega estamos en lineas")
+                        TestWidget("hola12")
                     ]
-                )
-            )
+                ),
+            ),
+        ),
+    )
 
-        page.update()
 
-    page.on_route_change = route_change
-    page.go(page.route)
-
-ft.app(main)
+ft.app(target=main)
